ad6a5ddd3d7f38d57507e5758745e5477ecfc3dc
eduwardoo-92 <eduwardoo_92@hotmail.com>
2024-10-07 03:04:43 -0700

    1st commit

---
+++ js/03-triangle.js
@@ -0,0 +1,9 @@
+/* Write a loop that displays the following triangle within a console window:
+
+#
+##
+###
+####
+#####
+######
+####### */
---
+++ js/04-odd-or-even.js
@@ -0,0 +1,8 @@
+/*
+Write a loop that will iterate from 0 to 15. For each iteration it will check if the current number is odd or even, and display a message in the console window.
+
+Sample Output:
+"0 is even"
+"1 is odd"
+"2 is even" */
+
